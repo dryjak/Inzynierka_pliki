cd3c1d81460e61fe5c9dcd5a03692b5bf3d42bae
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-10-29 23:26:16 +0100

    to do for tommorow

--- MPU6050_V1/Core/Src/main.c
+++ MPU6050_V1/Core/Src/main.c
@@ -99,6 +99,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+//todo: read acceleration and gyro data, plot results on the graph
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
