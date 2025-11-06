855390c7f994ab9fe67e797e0f121d1ff501bb8e
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-11-06 21:16:27 +0100

    added whoale servo function

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/60/a05ab09b4cbb00101ff19900f0c919e5
@@ -0,0 +1,8 @@
+/*
+ * SerwoSimple.c
+ *
+ *  Created on: Nov 6, 2025
+ *      Author: dryla
+ */
+
+
--- Serwomotor_V1/Debug/Core/Src/SerwoSimple.d
+++ Serwomotor_V1/Debug/Core/Src/SerwoSimple.d
@@ -1,4 +1,5 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/SerwoSimple.o: ../Core/Src/SerwoSimple.c \
+ ../Core/Inc/SerwoSimple.h ../Core/Inc/tim.h ../Core/Inc/main.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
  ../Core/Inc/stm32g4xx_hal_conf.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h \
@@ -27,9 +28,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h \
- ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h \
- ../Core/Inc/usart.h ../Core/Inc/main.h ../Core/Inc/tim.h \
- ../Core/Inc/gpio.h ../Core/Inc/SerwoSimple.h
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h
+../Core/Inc/SerwoSimple.h:
+../Core/Inc/tim.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h:
 ../Core/Inc/stm32g4xx_hal_conf.h:
@@ -60,8 +61,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h:
-../Core/Inc/usart.h:
-../Core/Inc/main.h:
-../Core/Inc/tim.h:
-../Core/Inc/gpio.h:
-../Core/Inc/SerwoSimple.h:
