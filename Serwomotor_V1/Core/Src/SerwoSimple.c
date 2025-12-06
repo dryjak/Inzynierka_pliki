4ae063c2d63d45bd017d201612c638c5a2f4ebed
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-12-06 13:22:49 +0100

    function to move servo with different velocity

--- Serwomotor_V1/Core/Src/SerwoSimple.c
+++ Serwomotor_V1/Core/Src/SerwoSimple.c
@@ -34,3 +34,25 @@ void SetAngle(uint16_t Angle, uint8_t Mode)
 	__HAL_TIM_SET_COMPARE(&WhichTimer, TimerChannel,Tmp);
 }
 
+void Servo_MoveSlowly(uint16_t startAngle, uint16_t endAngle, uint8_t speedDelay)
+{
+    if (startAngle < endAngle)
+    {
+        // Ruch w górę
+        for (uint16_t i = startAngle; i <= endAngle; i++)
+        {
+            SetAngle(i, 1);       // Ustaw mały krok
+            HAL_Delay(speedDelay); // Czekaj (im dłużej, tym wolniejszy ruch)
+        }
+    }
+    else
+    {
+        // Ruch w dół
+        for (uint16_t i = startAngle; i >= endAngle; i--)
+        {
+            SetAngle(i, 1);
+            HAL_Delay(speedDelay);
+        }
+    }
+}
+
