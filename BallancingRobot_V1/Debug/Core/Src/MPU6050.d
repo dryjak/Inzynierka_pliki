09a509969649d2bfa8454078bde66669944c51e9
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-12-09 14:02:55 +0100

    another to do - MPU6050

--- BallancingRobot_V1/Debug/Core/Src/MPU6050.d
+++ BallancingRobot_V1/Debug/Core/Src/MPU6050.d
@@ -1,5 +1,5 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+Core/Src/MPU6050.o: ../Core/Src/MPU6050.c ../Core/Inc/MPU6050.h \
+ ../Core/Inc/main.h ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Core/Inc/stm32f4xx_hal_conf.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
@@ -28,9 +28,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
- ../Core/Inc/i2c.h ../Core/Inc/main.h ../Core/Inc/tim.h \
- ../Core/Inc/gpio.h ../Core/Inc/motor_simple.h ../Core/Inc/Encoder.h \
- ../Core/Inc/PID.h ../Core/Inc/MPU6050.h
+ ../Core/Inc/main.h
+../Core/Inc/MPU6050.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -61,11 +60,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
-../Core/Inc/i2c.h:
 ../Core/Inc/main.h:
-../Core/Inc/tim.h:
-../Core/Inc/gpio.h:
-../Core/Inc/motor_simple.h:
-../Core/Inc/Encoder.h:
-../Core/Inc/PID.h:
-../Core/Inc/MPU6050.h:
