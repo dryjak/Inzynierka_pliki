17ee3bdc23c326b03f237683f4b3d427680287d8
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-12-30 09:34:57 +0100

    update encoder code

--- BallancingRobot_V1/Core/Inc/Encoder.h
+++ BallancingRobot_V1/Core/Inc/Encoder.h
@@ -12,19 +12,21 @@
 
 typedef struct
 {
-	TIM_HandleTypeDef *Tim;			//pointer to Timer
-	uint16_t PulsesPerRevolution;	//number of pulses per one motor revolution
-	float SampleTime;				//e.g. 0.01 for 100hz
+	TIM_HandleTypeDef *Tim;			// Handle to the timer used for the encoder
+	uint16_t PulsesPerRevolution;	// Number of pulses per one motor revolution
+	float SampleTime;				// Sampling period in seconds
 
-	uint32_t LastCounterValue;		//Encoder value in previous iteration
+	uint32_t LastCounterValue;		// Timer counter value from the previous iteration
 
-	volatile int64_t TotalPulses;	//Sum of all the impulses
-	float AngularVelocity;			//Calculated Angular Velocity
-	float Angle;					//Calculated angle
+	volatile int64_t TotalPulses;	// Total sum of pulses
+	float AngularVelocity;			// Calculated velocity in RPM
+	float Angle;					// Current wheel angle in degrees
 
 }Encoder_t;
 
+//Initialization
 void Encoder_Init(Encoder_t *Encoder, TIM_HandleTypeDef *Tim, uint16_t PulsesPerRevolution, float SampleTime);
+//Updating encoder values
 void Encoder_Update(Encoder_t *Encoder);
 
 
