b7fabd8395e5906ff4d78c902dc298b27b7d15de
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2026-01-11 22:35:24 +0100

    kalman filter liblary

---
+++ BallancingRobot_V1/Core/Inc/kalman_filter.h
@@ -0,0 +1,28 @@
+/*
+ * kalman_filter.h
+ *
+ *  Created on: Jan 11, 2026
+ *      Author: dryla
+ */
+
+#ifndef INC_KALMAN_FILTER_H_
+#define INC_KALMAN_FILTER_H_
+
+typedef struct {
+    float Q_angle;   // Szum procesu (dla kąta) - jak bardzo ufamy matematyce
+    float Q_bias;    // Szum procesu (dla biasu żyroskopu)
+    float R_measure; // Szum pomiaru - jak bardzo NIE ufamy akcelerometrowi (wibracje)
+
+    float angle;     // Obliczony kąt (wynik)
+    float bias;      // Obliczony dryft żyroskopu
+    float rate;      // Prędkość kątowa bez dryftu
+
+    float P[2][2];   // Macierz kowariancji błędu
+} Kalman_t;
+
+// Deklaracja instancji dla Pitch i Roll
+Kalman_t KalmanPitch;
+Kalman_t KalmanRoll;
+
+
+#endif /* INC_KALMAN_FILTER_H_ */
